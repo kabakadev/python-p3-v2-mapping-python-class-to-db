0ae0295909515d2aeac984727faf4194be636b0d
Ian Kabaka <iankabaka1@gmail.com>
2024-12-11 03:21:05 +0300

    passing tests for python class to db

--- lib/department.py
+++ lib/department.py
@@ -7,6 +7,62 @@ class Department:
         self.id = id
         self.name = name
         self.location = location
+    @classmethod
+    def create_table(cls):
+        """Create a new table to persist the attributes of Dpeartment instances"""
+        sql = """
+              CREATE TABLE IF NOT EXISTS departments (
+              id INTEGER PRIMARY KEY,
+              name TEXT,
+              location TEXT
+              ) 
+              """
+        CURSOR.execute(sql)
+        CONN.commit()
+    @classmethod
+    def drop_table(cls):
+        """Drop table that persists Department instances"""
+        sql = """
+             DROP TABLE IF EXISTS departments
+               """
+        CURSOR.execute(sql)
+        CONN.commit()
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+    def save(self):
+        """Insert a new row with the name and location of the current Department Instance.
+        Update object id attribute using the primary key value of new row.
+        """
+        sql = """
+        INSERT INTO departments (name,location) 
+        VALUES (?,?)  
+        """
+        CURSOR.execute(sql,(self.name,self.location))
+        self.id = CURSOR.lastrowid
+        CONN.commit()  
+    @classmethod
+    def create(cls, name, location):
+        """ Initialize a new Department instance and save the object to the database """
+        department = cls(name, location)
+        department.save()
+        return department
+    def update(self):
+        """Update the table row corresponding to the current Department instance."""
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+    def delete(self):
+        """Delete the table row corresponding to the current Department instance"""
+        sql = """
+            DELETE FROM departments
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+    
